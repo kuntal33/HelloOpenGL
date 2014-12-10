6f0023ef1446815314df386f5f322e6e78a74a72
Kuntal Gajjar <kuntalgajjar@michaels-Mac-mini.local>
2014-12-10 13:01:26 -0500

    Initial Commit

---
+++ HelloOpenGL/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloOpenGL
+//
+//  Created by Kuntal Gajjar on 2014-12-10.
+//  Copyright (c) 2014 AppetiteLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloOpenGL/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloOpenGL
+//
+//  Created by Kuntal Gajjar on 2014-12-10.
+//  Copyright (c) 2014 AppetiteLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
